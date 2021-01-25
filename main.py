6a11c5ab4ab94e0ac1d285255a8699fbbaa9e6a4
Diana <lix_diana@yahoo.com>
2021-01-25 18:12:28 +0200

    My first project commit.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-initial_list = [7, 8, 9, 8.3, 2, 3, 1, 4, 10, 5, 6]
+initial_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 
 ascending_list = initial_list.copy()
 ascending_list.sort()
@@ -7,6 +7,5 @@ print('sort asc: ', ascending_list)
 descending_list = list(ascending_list[::-1])
 # descending_list.reverse()
 print('reverse desc: ', descending_list)
-print('initial list: ', initial_list)
 print('odd list: ', ascending_list[::2])
 print('multiple of 3 list: ', ascending_list[2::3])
